6bdce5447649f4f9717db775238fcae4ce3c3398
Brad McCollum <btmcco8@gmail.com>
2018-08-17 23:41:46 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     # self.split(/[?\.!]+/).count
-    self.split(/[?\.!]/).delete_if {|x| x  == “” }
+    self.split(/[?\.!]/).delete_if {|x| x  == "" }
   end
 
 end
